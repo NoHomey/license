559831133b7918051b28ad62bd562bd835cf403a
NoHomey <ivo_stratev@abv.bg>
2015-09-21 22:57:24 +0300

    don't use unknwown deps

--- ang/public/javascripts/app.js
+++ ang/public/javascripts/app.js
@@ -1,22 +1,54 @@
+angular.module('app.servecices', [])
+  .factory('appServe', ['$http', '$q', function ($http, $q) {
+    var appServe = {};
+    appServe.get = function() {
+      var deferred = $q.defer();
+      $http.get('/data').then(function (data) {
+        deferred.resolve(data.data);
+
+      });
+      return deferred.promise;
+    }
+    appServe.t = function (f) {
+      appServe.get().then(f);
+    };
+    return appServe;
+  }])
 
 
-angular.module('controllers', [])
-  .controller('LoginCtrl', ['$scope', '$location', function ($scope, $location) {
-    $scope.name= "John";
-    $scope.pass= "Doe";
-  }]);
+angular.module('app.controllers', [])
+    .controller('LoginCtrl', ['$scope', '$location', function ($scope, $location) {
+      $scope.login = function () {
 
-angular.module('app', ['controllers', 'ngRoute'])
-  .config(['$routeProvider', '$httpProvider', function ($httpProvider, $routeProvider) {
-    $routeProvider.when('/', {
-      controller : 'LoginCtrl',
-      templateUrl : '/view-login.html'
-      //reqLogin : false
-    })
-    /*.when('', {
-      contorller : 'IndexCtrl',
-      templateUrl : '/index.html',
-      reqLogin : true
-    })*/
-    .otherwise({redirectTo : '/login'});
-}]);
+      };
+      $scope.name= "John";
+      $scope.pass= "Doe";
+    }])
+    .controller('AppCtrl', ['$scope', 'appServe', function ($scope, appServe) {
+      appServe.t(function (data) {
+        $scope.data = data.value;
+      });
+    }])
+    .controller('RegCtrl', ['$scope', '$location', function ($scope, $location) {
+
+    }])
+
+angular.module('app', ['app.controllers', 'app.servecices', 'ngRoute']).config(['$routeProvider', function ($routeProvider) {
+      $routeProvider
+        .when('/login', {
+          controller : 'LoginCtrl',
+          templateUrl : '/view-login.html',
+      })
+      .when('/', {
+          controller : 'AppCtrl',
+          templateUrl : '/view-app.html',
+      })
+       .when('/register', {
+          controller : 'RegCtrl',
+          templateUrl : '/view-reg.html',
+      })
+      .otherwise({redirectTo : '/login'});
+  }])
+  .run(function ($location) {
+     true ? $location.path('/') :  $location.path('/login');
+});
--- ang/routes/index.js
+++ ang/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send(html('index'));
 });
 
+router.get('/view-app.html', function(req, res, next) {
+  res.send(html('view-app'));
+});
+
+router.get('/view-login.html', function(req, res, next) {
+  res.send(html('view-login'));
+});
+
+router.get('/data', function(req, res, next) {
+  res.send({value : ['ivo', 'aa', 'dfdfd']});
+});
+
+
 module.exports = router;
